639936c8303a7ceff30fbbc6cfbca0bf464e628e
mouhamednoire <mouhamedabdallah.bathilly@uvs.edu.sn>
2024-09-06 19:44:59 +0000

    revision of the dashboard creation with the header and the sidembar"

--- src/app/layout/dashboard/dashboard.module.ts
+++ src/app/layout/dashboard/dashboard.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SharedModule} from "../../shared/shared.module";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {HeaderComponent} from "../header/header.component";
+import {SidebarComponent} from "../sidebar/sidebar.component";
 
 
 
@@ -14,7 +16,9 @@ import {DashboardComponent} from "./dashboard/dashboard.component";
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    HeaderComponent,
+    SidebarComponent
   ]
 })
 export class DashboardModule { }
