febf75bbb90d5932f7b1d5c4a20ec7db14f0c2a0
abduRahman49 <sabdouseye@gmail.com>
2024-09-12 01:16:42 +0000

    Ajout: stylisation page registration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    LoginComponent
+    LoginComponent,
+    RegistrationComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
