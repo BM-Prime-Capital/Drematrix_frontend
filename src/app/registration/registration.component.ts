f16c3fe85a2702b141a30ce4c1579c1c0401983f
abduRahman49 <sabdouseye@gmail.com>
2024-09-12 09:41:04 +0000

    Fix: Ajout partie formulaire de connexion school

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [
-    MatInputModule,
-    MatFormFieldModule,
-    MatIconModule
-  ],
+  imports: [],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss',
 })
