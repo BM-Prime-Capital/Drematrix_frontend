157820d225bf18992f5b0880896b0f3a658983d5
abduRahman49 <sabdouseye@gmail.com>
2024-09-12 11:35:41 +0000

    Debugging dashboard styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { PreLoginComponent } from './pre-login/pre-login.component';
+import { DashboardComponent } from './layout/dashboard/dashboard/dashboard.component';
+
 
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
   { path: 'pre-login', component: PreLoginComponent },
   { path: 'registration', component: RegistrationComponent }
--- src/app/layout/dashboard/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard/dashboard.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
 
