60edcc978eb35c629f2f68fef52907dca97bc4b4
abduRahman49 <sabdouseye@gmail.com>
2024-09-19 23:32:07 +0000

    Feature: Loading component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
 import {FormsModule, NgForm} from "@angular/forms";
+import { LoaderComponent } from '../loader/loader.component';
 
 
 @Component({
@@ -12,28 +13,32 @@ import {FormsModule, NgForm} from "@angular/forms";
     RouterOutlet,
     RouterLink,
     FormsModule,
+    LoaderComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
-  constructor(private authService: AuthenticationService, private router: Router){
+  constructor(private authService: AuthenticationService, private router: Router){}
 
-  }
+  isLoading : boolean = false;
 
   onSubmit(loginForm: NgForm) {
     const {email, password} = loginForm.value;
     console.log(email, password)
+    this.isLoading = true;
     this.authService.login(email, password).subscribe(
       {
         next: (response) =>{
           console.log('Login successful, token:', response.token);
+          this.isLoading = false;
           // You can store the token, like in localStorage
           this.router.navigate(['/home']);
         },
         error : (err)=>{
           console.log('login failled', err)
+          this.isLoading = false;
         }
       }
     );
