f050d8c1503510b3e5ebe1d02aed0201efd249c8
abduRahman49 <sabdouseye@gmail.com>
2024-09-20 12:57:14 +0000

    Refactor: removed loading state when debbuging

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -23,23 +23,23 @@ export class LoginComponent {
 
   constructor(private authService: AuthenticationService, private router: Router, private toastService: ToastService){}
 
-  isLoading : boolean = true;
+  isLoading : boolean = false;
 
   onSubmit(loginForm: NgForm) {
     const {email, password} = loginForm.value;
     console.log(email, password)
-    // this.isLoading = true;
+    this.isLoading = true;
     this.authService.login(email, password).subscribe(
       {
         next: (response) =>{
           console.log('Login successful, token:', response.token);
-          // this.isLoading = false;
+          this.isLoading = false;
           // You can store the token, like in localStorage
           this.router.navigate(['/home']);
         },
         error : (err)=>{
           console.log('login failled', err)
-          // this.isLoading = false;
+          this.isLoading = false;
           this.toastService.showErrorMessage("Please verify your credentials");
         }
       }
